18107b99a53bc0a81f4bf8ddbb3e4363acc3ecc4
MaxVargas <mxvrgs@gmail.com>
2025-03-27 22:16:43 -0700

    add: vector operations

--- src/vec.rs
+++ src/vec.rs
@@ -1,7 +1,7 @@
 use std::ops;
 
 // https://codereview.stackexchange.com/questions/256345/n-dimensional-array-in-rust
-#[derive(PartialEq,Debug)]
+#[derive(PartialEq,Debug,Clone)]
 pub struct VecNd<const N: usize> {
     data: Vec<f32>,
 }
@@ -14,6 +14,16 @@ impl<const N: usize> VecNd<N> {
     }
 }
 
+impl<const N: usize> VecNd<N> {
+    fn dot(&self, v: &VecNd<N>) -> f32 {
+        (self.clone() * v.clone()).data.iter().sum()
+    }
+
+    fn norm(&self) -> f32 {
+        self.dot(&self).powf(0.5)
+    }
+}
+
 impl<const N: usize> ops::Add<VecNd<N>> for VecNd<N> {
     type Output = VecNd<N>;
 
@@ -21,6 +31,42 @@ impl<const N: usize> ops::Add<VecNd<N>> for VecNd<N> {
         let mut sum = vec![0.0; N];
         for i in 0..N {
             sum[i] = self.data[i] + rhs.data[i];
+        }
+        VecNd::from_vec(sum)
+    }
+}
+
+impl<const N: usize> ops::Sub<VecNd<N>> for VecNd<N> {
+    type Output = VecNd<N>;
+
+    fn sub(self, rhs: VecNd<N>)-> Self::Output {
+        let mut sum = vec![0.0; N];
+        for i in 0..N {
+            sum[i] = self.data[i] - rhs.data[i];
+        }
+        VecNd::from_vec(sum)
+    }
+}
+
+impl<const N: usize> ops::Mul<VecNd<N>> for VecNd<N> {
+    type Output = VecNd<N>;
+
+    fn mul(self, rhs: VecNd<N>)-> Self::Output {
+        let mut sum = vec![0.0; N];
+        for i in 0..N {
+            sum[i] = self.data[i] * rhs.data[i];
+        }
+        VecNd::from_vec(sum)
+    }
+}
+
+impl<const N: usize> ops::Mul<VecNd<N>> for f32 {
+    type Output = VecNd<N>;
+
+    fn mul(self, rhs: VecNd<N>)-> Self::Output {
+        let mut sum = vec![0.0; N];
+        for i in 0..N {
+            sum[i] = self * rhs.data[i];
         }
         VecNd::from_vec(sum)
     }
@@ -52,32 +98,3 @@ mod tests {
         );
     }
 }
-
-// TODO: Implement these and make tests
-// fn sub(v1: &Vec<f32>, v2: &Vec<f32>) -> Vec<f32> {
-//     let mut sum = vec![];
-//     let n = v1.len();
-//     for i in 0..n {
-//         sum.push(v1[i] - v2[i]);
-//     }
-//     sum
-// }
-// fn scale(v1: &Vec<f32>, s: f32) -> Vec<f32> {
-//     let mut v2 = vec![];
-//     let n = v1.len();
-//     for i in 0..n {
-//         v2.push(v1[i] * s);
-//     }
-//     v2
-// }
-// fn dot(v1: &Vec<f32>, v2: &Vec<f32>) -> f32 {
-//     let mut sum = 0.0;
-//     let n = v1.len();
-//     for i in 0..n {
-//         sum += v1[i] * v2[i];
-//     }
-//     sum
-// }
-// fn norm(v1: &Vec<f32>) -> f32 {
-//     dot(v1, v1).pow(0.5)
-// }
